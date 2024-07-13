1f0bd5d4cec326f19f491f42f7102f842cf2d624
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-07-13 08:15:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+//your JS code here. If requir
+    let modal = document.getElementById("myModal");
+    let btn = document.getElementById("openModal");
+    let span = document.getElementsByClassName("close-modal")[0];
+
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
